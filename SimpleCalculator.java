585d30ff55a3b3455ce515c77cfa4f57d1a33e71
Isah Jacob <isahjacob0@gmail.com>
2022-06-29 19:17:27 +0100

    A simple calculator built with Java that can add, multiply, divide and minus.

---
+++ SimpleCalculator.java
@@ -0,0 +1,17 @@
+public class SimpleCalculator {
+    public int add(int numberA, int numberB){
+        return numberA + numberB;
+    }
+
+    public int division(int numberA, int numberB){
+        return numberA / numberB;
+    }
+
+    public  int multiplication(int numberA, int numberB){
+        return numberA * numberB;
+    }
+
+    public int subtractNumbers(int numberA, int numberB) {
+        return numberA - numberB;
+    }
+}
